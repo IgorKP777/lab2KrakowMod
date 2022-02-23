ab2ef3e28d61c228417f7355c2f6afc7a4f72f51
Admin <igorkydryashov7k@gmail.com>
2022-02-23 13:44:39 +0800

    лаба2 Моделирование

--- main.py
+++ main.py
@@ -1,60 +1,49 @@
 import random as r
 import math as m
-
+import numpy as np
+import statistics as stat
 from matplotlib import pyplot as plt
-from statistics import mean as average
-
-n = 10000
-mo = 10000
-kv = 0.1
 
 if __name__ == '__main__':
-
-    selectionNormalDistribution = []
-    selectionLogarithmicallyNormalDistribution = []
+    n = 10000
+    mo = 10000
+    kv = 0.1
 
     # нормальное распределение
-    for i in range(n):
-        z = 0.0
-        for y in range(12):
-            z += r.uniform(0, 1)
-        z -= 6
-        x1 = mo + mo * kv * z
-        selectionNormalDistribution.append(x1)
-
-    averageN = average(selectionNormalDistribution)
-    sumX2N = 0
-    for element in selectionNormalDistribution:
-        sumX2N += element ** 2
-
-    varianceN = (sumX2N - n * averageN * averageN) / (n - 1)
-    standardDeviationN = m.sqrt(varianceN)
-    sigmaN = (1.96 * standardDeviationN) / (m.sqrt(n))
-    x1N = averageN - sigmaN
-    x2N = averageN + sigmaN
-    print('----- Нормальный закон -----')
-    print('m =', mo, 'мат ожидание')
-    print('kv =', kv, 'коэффициент вариации')
-    print('n =', n, 'размер выборки')
-    print('оценка мат ожидания\n', round(averageN, 3))
-    print('оценка дисперсии\n', round(varianceN, 3))
-    print('оценка среднеквадратического отклонения\n', round(standardDeviationN, 3))
-    print('интервальная оценка математического ожидания\n', '(', round(x1N, 3), ',', round(x2N, 3), ')')
-    print('точечная оценка математического ожидания\n', round((x1N + x2N) / 2, 3))
-    print('\n', '\n')
-
-    minN = m.floor(min(selectionNormalDistribution) / 1000) * 1000
-    maxN = m.ceil(max(selectionNormalDistribution) / 1000) * 1000
-
-    frequencyN = [i for i in range(minN, maxN, 200)]
-
-    plt.hist(selectionNormalDistribution, bins=frequencyN, edgecolor='white')
-    plt.title('Нормальный закон')
-    plt.xlabel('')
-    plt.ylabel('Частота')
-    plt.show()
+    # selectionNormalDistribution = list(np.random.normal(loc=mo, scale=1000, size=n))
+    # averageN = stat.mean(selectionNormalDistribution)
+    # varianceN = stat.variance(selectionNormalDistribution)
+    #
+    # standardDeviationN = m.sqrt(varianceN)
+    #
+    # sigmaN = (1.96 * standardDeviationN) / (m.sqrt(n))
+    # x1N = averageN - sigmaN
+    # x2N = averageN + sigmaN
+    # print('----- Нормальный закон -----')
+    # print('m =', mo, '(мат ожидание)')
+    # print('kv =', kv, '(коэффициент вариации)')
+    # print('n =', n, '(размер выборки)')
+    # print('Оценка мат ожидания\n', round(averageN, 3), sep='')
+    # print('Оценка дисперсии\n', round(varianceN, 3), sep='')
+    # print('Оценка среднеквадратического отклонения\n', round(standardDeviationN, 3), sep='')
+    # print('Интервальная оценка математического ожидания\n', '(', round(x1N, 3), ', ', round(x2N, 3), ')', sep='')
+    # print('Точечная оценка математического ожидания\n', round((x1N + x2N) / 2, 3), sep='')
+    # print('\n', '\n')
+    #
+    # minN = m.ceil(min(selectionNormalDistribution) / 1000) * 1000
+    # maxN = m.floor(max(selectionNormalDistribution) / 1000) * 1000
+    #
+    # frequencyN = [i for i in range(minN, maxN, 200)]
+    #
+    # graphN = plt
+    # graphN.hist(selectionNormalDistribution, bins=frequencyN, edgecolor='white')
+    # graphN.title('Нормальный закон')
+    # graphN.xlabel('')
+    # graphN.ylabel('Частота')
+    # graphN.show()
 
     # логарифмически нормальное распределение
+    selectionLogarithmicallyNormalDistribution = []
     beta = m.sqrt(m.log(1 + kv ** 2))
     alfa = m.log(mo - beta ** 2 / 2)
     for i in range(n):
@@ -65,13 +54,11 @@ if __name__ == '__main__':
         y = alfa + z2 * beta
         x2 = m.exp(y)
         selectionLogarithmicallyNormalDistribution.append(x2)
+    # selectionLogarithmicallyNormalDistribution = list(np.random.lognormal(mean=mo, size=n))
 
-    averageL = average(selectionLogarithmicallyNormalDistribution)
-    sumX2L = 0
-    for element in selectionLogarithmicallyNormalDistribution:
-        sumX2L += element ** 2
+    averageL = stat.mean(selectionLogarithmicallyNormalDistribution)
 
-    varianceL = (sumX2L - n * averageL * averageL) / (n - 1)
+    varianceL = stat.variance(selectionLogarithmicallyNormalDistribution)
     standardDeviationL = m.sqrt(varianceL)
     sigmaL = (1.96 * standardDeviationL) / (m.sqrt(n))
     x1L = averageL - sigmaL
